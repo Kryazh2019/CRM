90fd33fc36cf58e4a34056a592eb5455feb0bbef
unknown <Kryaz2018@gmail.com>
2019-03-20 20:30:05 +0300

    add CRM

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Ilya:qwertyIlya1998@fullstack-dvgzr.mongodb.net/test?retryWrites=true',
+    jwt: 'dev-jwt'
+}
---
+++ middleware/upload.js
@@ -0,0 +1,10 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const storage = multer.diskStorage({
+    destination(req, file)
+})
+
+module.exports = multer({
+
+})
